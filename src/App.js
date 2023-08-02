d030d94e0bc4b19d069a26a0cf8f9ba510710bc0
RobinHaljak <haljak.robin@gmail.com>
2023-08-02 20:09:30 +0100

    Initialising React app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Greeting from './Greeting';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello, React!</h1>
+        <Greeting name="Alice" />
+        <Greeting name="Bob" />
+        <Greeting name="Yuan" />
       </header>
     </div>
   );
