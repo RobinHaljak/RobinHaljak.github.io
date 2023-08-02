d7a8ef58ec80e7b560d28a448d2c70acfe7f2f60
RobinHaljak <haljak.robin@gmail.com>
2023-08-02 20:12:37 +0100

    removed index.html

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, React!</h1>
+        <h1>Hello, World!</h1>
         <Greeting name="Alice" />
         <Greeting name="Bob" />
         <Greeting name="Yuan" />
